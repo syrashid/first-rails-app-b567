2cb9619d9cd0bf1ba49655e4ecbfba946ae02885
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-22 03:14:47 -0500

    FIX: Small spacing fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'team', to: 'pages#team'
+
   # Generic Syntax
   # verb 'path', to: 'controller#action'
 end
